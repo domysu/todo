5827d8b5a4b7f0900b67ceabb0a17dbb3e356ee6
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-08-20 19:53:00 +0200

    Add page /todo

--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d7a8513d3e2b9e289ba051100596b061e70506")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
