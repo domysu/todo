ec60d3f2834a9cd414c581a4762dc07f24c9371c
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-09-02 23:02:59 +0300

    Some changes to front end..

--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459f7442ff771a2065d277ea852678f4b80fa016")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a91e7e641e9eea295188850ea02c7830cc421f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
